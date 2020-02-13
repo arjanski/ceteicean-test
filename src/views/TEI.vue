b6da45283f01f8fe9f82e0819325cd1fd246a434
Arjan Dhupia <arjanski@posteo.dea.dhupia@uni-koeln.de>
2020-02-13 23:04:35 +0100

    Initial app setup (Vue 2.6, Vue Router, CETEIcean)

---
+++ src/views/TEI.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    TEI would go here...
+    <div id="tei" v-html="teiHTML" />
+  </div>
+</template>
+
+<script>
+import CETEI from '../ceteicean/CETEI.js';
+export default {
+  name: 'TEITest',
+  data() {
+    return {
+      teiHTML: null
+    }
+  },
+  created() {
+    var CETEIcean = new CETEI()
+    CETEIcean.getHTML5("testTEI.xml", function(data) {
+      console.log(data)
+      // this.teiHTML = data
+    })
+  }
+}
+</script>
