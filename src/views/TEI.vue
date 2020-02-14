a95912d83b631b538ec25bd906325a3648efb007
Arjan Dhupia <arjanski@posteo.dea.dhupia@uni-koeln.de>
2020-02-15 00:24:27 +0100

    Working library, added CSS, added V-Runtime-Template, V-Tooltip plugins

--- src/views/TEI.vue
+++ src/views/TEI.vue
@@ -1,24 +1,30 @@
 <template>
-  <div>
-    TEI would go here...
-    <div id="tei" v-html="teiHTML" />
-  </div>
+<div>
+    <v-runtime-template :template="test"></v-runtime-template>
+    <div id="tei" v-html="TEI" />
+</div>
 </template>
 
 <script>
+import VRuntimeTemplate from "v-runtime-template";
 import CETEI from '../ceteicean/CETEI.js';
 export default {
   name: 'TEITest',
+  components: {
+    VRuntimeTemplate
+  },
   data() {
     return {
-      teiHTML: null
+      TEI: null,
+      test: `<button v-tooltip="TEI">Vue Markup & Scope Test</button>`
     }
   },
   created() {
     var CETEIcean = new CETEI()
-    CETEIcean.getHTML5("testTEI.xml", function(data) {
-      console.log(data)
-      // this.teiHTML = data
+    var self = this;
+    CETEIcean.getHTML5("https://gregorovius-edition.dhi-roma.it/api/letters/G000003", function(data) {
+      console.log(data.outerHTML)
+      self.TEI = data.outerHTML
     })
   }
 }
